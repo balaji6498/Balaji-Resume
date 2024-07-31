f40d7cec53206221075e37d14657fe8edd3001dd
Balaji SG <73786377+balaji6498@users.noreply.github.com>
2024-07-31 21:04:38 +0530

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,7 @@
+var typed = new Typed(".multiple-text",{
+    strings:["Game Tester","Data Center Engineer","Game Script Writer","Grey Hat Hacker"],
+    typeSpeed: 100,
+    backSpeed: 100,
+    backDelay: 1000,
+    loop: true
+})
